df87cf1f5055e4c49492fd9443c98956fb974128
edleijnse <ed@leijnse.info>
2025-03-10 17:24:07 +0100

    updateImage: sponsorType added to enable styles.css sponsorType gold-style, silver-style and platinum-style

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ const images = [
     { src: "images/gold/gold3.png", sponsortype: "gold" },
     { src: "images/silver/silver1.png", sponsortype: "silver" },
     { src: "images/silver/silver2.png", sponsortype: "silver" },
-    { src: "images/platinum/silver3.png", sponsortype: "platinum" },
+    { src: "images/platinum/adcubum.svg", sponsortype: "platinum" },
     { src: "images/platinum/mimacom.png", sponsortype: "platinum" }
 ];
 
@@ -18,9 +18,10 @@ let platinumIndex = 0;
 
 // Ablaufsteuerung in der Animation
 let currentSponsortype = "gold";
-function updateImage(newSrc) {
+function updateImage(newSrc, sponsorType) {
     const imageContainer = document.getElementById("bouncing-image");
     if (imageContainer) {
+        imageContainer.className = sponsorType;
         imageContainer.src = newSrc + "?" + new Date().getTime(); // Cache-busting
     } else {
         console.error("Image container not found!");
@@ -30,7 +31,7 @@ function updateImage(newSrc) {
 // Funktion für "gold"-Sponsoren
 function changeImageGold() {
     const goldImages = images.filter(image => image.sponsortype === "gold");
-    updateImage(goldImages[goldIndex].src );
+    updateImage(goldImages[goldIndex].src, "gold-style" );
     if (goldIndex === goldImages.length - 1) {
         currentSponsortype = "silver"; // Als Nächstes aufrufen}
     }
@@ -40,9 +41,8 @@ function changeImageGold() {
 // Funktion für "silver"-Sponsoren
 function changeImageSilver() {
     const silverImages = images.filter(image => image.sponsortype === "silver");
-    updateImage(silverImages[silverIndex].src );
-
-    if (silverIndex === silverIndex.length - 1) {
+    updateImage(silverImages[silverIndex].src ,"silver-style");
+    if (silverIndex === silverImages.length - 1) {
         currentSponsortype = "platinum";
     }// Als Nächstes aufrufen}
     silverIndex = (silverIndex + 1) % silverImages.length; // Index zyklisch erhöhen
@@ -53,8 +53,9 @@ function changeImageSilver() {
 
 function changeImagePlatinum() {
     const platinumImages = images.filter(image => image.sponsortype === "platinum");
-    updateImage(platinumImages[platinumIndex].src );
-    if (platinumIndex === platinumIndex.length - 1) {
+    updateImage(platinumImages[platinumIndex].src ,"platinum-style");
+    const imageContainer = document.getElementById("bouncing-image");
+    if (platinumIndex === platinumImages.length - 1) {
         currentSponsortype = "gold";
     }
     platinumIndex = (platinumIndex + 1) % platinumImages.length; // Index zyklisch erhöhen
